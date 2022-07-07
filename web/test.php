31334020d81f85fc187d38a4e589c3cd41dbf92d
yinMinz123 <20039202@myrp.edu.sg>
2022-07-07 12:56:10 +0800

    Update

--- web/test.php
+++ web/test.php
@@ -1,7 +1,18 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://trusted.example.com/path/jsonp?callback=alert(document.domain)//"></script>
+    <title>My First Heroku</title>
+    <style>
+         p {
+            font-size: 14px;
+            padding: 48px;
+            margin: 8px;
+        }
+        </style>
 </head>
 <body>
-  Hello World!
+  <p>Hello World!</p>
 </body>
 </html>
