2fdfc9acd2105258d0dec770748de4205ea57207
yinMinz123 <20039202@myrp.edu.sg>
2022-07-07 14:43:32 +0800

    Update

--- web/test.php
+++ web/test.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://trusted.example.com/path/jsonp?callback=alert(document.domain)//"></script>
     <title>My First Heroku</title>
     <style>
          p {
@@ -13,6 +12,7 @@
         </style>
 </head>
 <body>
+  <script src="https://trusted.example.com/path/jsonp?callback=alert(document.domain)//"></script>
   <p>Hello World!</p>
 </body>
 </html>
